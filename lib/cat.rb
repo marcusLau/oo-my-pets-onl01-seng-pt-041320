ebdb7c67cb2bf6a2565ec5deec4b756c62603b0b
Marcus Lau <lau.marcus76@gmail.com>
2020-05-11 22:25:45 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,12 +1,13 @@
 class Cat
   # code goes here
   
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
   
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
   
 end
