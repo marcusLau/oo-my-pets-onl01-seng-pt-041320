9a1513a6e2874415142585e3842c1f9116d2d4f3
Marcus Lau <lau.marcus76@gmail.com>
2020-05-12 01:48:46 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
 class Cat
   # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,90 @@
 class Owner
   # code goes here
+  
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @pets = {:cats => [], :dogs => []}
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    # returns an array of all cats who have the same owner
+    owned_cats = []
+    Cat.all.map do |cat|
+      if cat.owner == self 
+        owned_cats << cat
+      end
+    end
+    owned_cats
+  end
+  
+  def dogs
+    owned_dogs = []
+    Dog.all.map do |doge|
+      if doge.owner == self
+        owned_dogs << doge
+      end
+    end
+    owned_dogs
+  end
+  
+  def buy_cat(cat)
+    @pets[:cats] << Cat.new(cat, self)
+  end
+  
+  def buy_dog(dog)
+    @pets[:dogs] << Dog.new(dog, self)
+  end
+  
+  # makes ALL the dog's moods happy
+  # iterate thru @pets[:dogs] and change mood
+  def walk_dogs
+    Dog.all.map do |doge|
+      doge.mood = "happy"
+    end
+  end
+  
+  def feed_cats
+    Cat.all.map do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  # sells all the pets which makes their mood nervous
+  def sell_pets 
+    Cat.all.map do |cat| 
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    Dog.all.map do |dog| 
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+  
+  def list_pets
+    "I have #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+  
 end
