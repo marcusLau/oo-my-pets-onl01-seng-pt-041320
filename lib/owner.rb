01291a4b5e8f648d63f41f97bedc51262476d0ca
Marcus Lau <lau.marcus76@gmail.com>
2020-05-12 01:48:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -84,7 +84,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{@owner.dogs.count} dog(s), and #{Cat.all} cat(s)."
+    "I have #{@pets[:dogs].length} dog(s), and #{Cat.all} cat(s)."
   end
   
   
